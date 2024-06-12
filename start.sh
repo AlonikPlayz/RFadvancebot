14f5187cb11a82fa936045962b841ba56a359ebd
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2024-06-12 17:38:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/RFadvancebot /AlonikPlayz
-cd /AlonikPlayz
+git clone https://github.com/RFadvancebot /LazyDeveloper
+cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py

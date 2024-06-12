aabb9a2def979cb0cac6ae50c537245f0b4c19e8
AlonikPlayz <149935601+AlonikPlayz@users.noreply.github.com>
2024-06-12 17:25:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-gunicorn app:app & python3 bot.py
+echo "Cloning Repo...."
+git clone https://github.com/RFadvancebot /AlonikPlayz
+cd /AlonikPlayz
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
